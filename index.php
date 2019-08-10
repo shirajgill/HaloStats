f27ac59b649284c5e5b971a7c6cdb5a4c461bef2
Shiraj Gill <gillshiraj@hotmail.com>
2019-08-10 18:43:44 -0400

    f

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ $json_data = json_decode($json, true);
 
 foreach ($json_data["Routes"] as $route) {
 	if (strpos(strtolower($_SERVER['REQUEST_URI']), strtolower($route["route"])) !== false) {
-		echo "__" . $route;
+		echo "__" . $route; exit();
 		include $route["file"];
 		call_user_func($route["function"]);
 	}
