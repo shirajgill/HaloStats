885afad457905968317ee85c701c55872526d4c9
Shiraj Gill <gillshiraj@hotmail.com>
2019-08-10 18:41:16 -0400

    f

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ $json_data = json_decode($json, true);
 
 foreach ($json_data["Routes"] as $route) {
 	echo $_SERVER['REQUEST_URI'];
-	echo "__" . $_SERVER['REQUEST_URI'];
+	echo "__" . $_SERVER['REQUEST_URI']; die;
 	if (strpos(strtolower($_SERVER['REQUEST_URI']), strtolower($route["route"])) !== false) {
 		include $route["file"];
 		call_user_func($route["function"]);
